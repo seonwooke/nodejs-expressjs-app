13064af2e298f40e6c6596970ed0f38e1f170020
seonwooke <seonookkim@gmail.com>
2023-05-18 02:28:23 +0900

    :art: Route
    Route

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express');
-const usersController = require('./controllers/users.controller');
-const postsController = require('./controllers/posts.controller');
 const PORT = 4000;
 
-const app = express();
-app.use(express.json());
+const usersRouter = require('./routes/users.router');
+const postsRouter = require('./routes/posts.router');
 
+const app = express();
+
+app.use(express.json());
 app.use((req, res, next) => {
   const start = Date.now();
   console.log(`start: ${req.method} ${req.url}`);
   next();
   const diffTime = Date.now() - start;
-  console.log(`end: ${req.method} ${req.url} ${diffTime}ms`);
+  console.log(`end: ${req.method} ${req.baseUrl}${req.url} ${diffTime}ms`);
 });
 
-app.get('/users', usersController.getUsers);
-app.get('/users/:userId', usersController.getUser);
-app.post('/users', usersController.postUser);
-
-app.get('/posts', postsController.getPost);
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
